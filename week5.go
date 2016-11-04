7a61a6c8789e64dd02b5d41fea1d5534d7dd96a8
Andrew Ivanov <ivandralex@gmail.com>
2016-11-04 19:25:09 +0300

    Dijkstra naive implementation

--- week5.go
+++ week5.go
@@ -23,8 +23,6 @@ func ReadEdges(path string) (*map[int][]*Vertex, *[]int) {
 		os.Exit(1)
 	}
 
-	//graph := [][]int{}
-
 	adjacencyMap := make(map[int][]*Vertex)
 	vertices := []int{}
 
@@ -55,8 +53,6 @@ func ReadEdges(path string) (*map[int][]*Vertex, *[]int) {
 
 			edgeParts := strings.Split(str, ",")
 
-			//fmt.Printf("edgeParts: %v\n", edgeParts)
-
 			edge := new(Vertex)
 			edge.head, _ = strconv.Atoi(edgeParts[0])
 			edge.length, _ = strconv.Atoi(edgeParts[1])
@@ -72,8 +68,82 @@ func ReadEdges(path string) (*map[int][]*Vertex, *[]int) {
 }
 
 func main() {
-	adjacencyMap, _ := ReadEdges("./data/dijkstra.txt")
-	fmt.Printf("Finished reading edges: %v\n", (*adjacencyMap)[1])
+	adjacencyMap, vertices := ReadEdges("./data/dijkstra.txt")
+	fmt.Printf("Finished reading edges")
 
-	fmt.Printf("10 shortest distances:\n")
+	minPaths := findMinPaths(1, adjacencyMap, vertices)
+
+	targetVertices := []int{7, 37, 59, 82, 99, 115, 133, 165, 188, 197}
+
+	var length int
+	var targetPaths []string
+
+	for _, v := range targetVertices {
+		if _, ok := (*minPaths)[v]; ok {
+			length = (*minPaths)[v]
+		} else {
+			length = 1000000
+		}
+		targetPaths = append(targetPaths, strconv.Itoa(length))
+	}
+
+	pathsStr := strings.Join(targetPaths, ",")
+
+	fmt.Printf("10 shortest distances: %s\n", pathsStr)
+}
+
+//findMinPaths fins min paths for all vertices reachable from source vertex
+func findMinPaths(source int, adjacencyMap *map[int][]*Vertex, vertices *[]int) *map[int]int {
+	//Maps vertex to the lendth of the shortest path to this vertex
+	minPaths := make(map[int]int)
+	minPaths[source] = 0
+
+	//Vertices with assigned shortest path lengths
+	g := []int{source}
+
+	//Vertices that do not belong to the frontier
+	innerVertices := make(map[int]bool)
+
+	var minPath int
+	var minVertex int
+	var onFrontier bool
+
+	for len(g) < len(*vertices) {
+		//Assign minPath with max possible length
+		minPath = 1000000
+
+		//Add new vertex to g
+		for _, v := range g {
+			//If all v neighbours already added to g
+			if _, ok := innerVertices[v]; ok {
+				continue
+			}
+
+			onFrontier = false
+
+			//Griddy vertex selction
+			for _, vertex := range (*adjacencyMap)[v] {
+				if _, ok := minPaths[vertex.head]; ok {
+					continue
+				}
+
+				onFrontier = true
+
+				if minPaths[v]+vertex.length < minPath {
+					minPath = minPaths[v] + vertex.length
+					minVertex = vertex.head
+				}
+			}
+
+			//v is no longer on frontier
+			if !onFrontier {
+				innerVertices[v] = true
+			}
+		}
+
+		minPaths[minVertex] = minPath
+		g = append(g, minVertex)
+	}
+
+	return &minPaths
 }
